4504fb59741f9cff10f934ab14c654c36d1ea9b2
ph4ntom5 <tomsstals1@gmail.com>
2021-09-17 16:01:32 +0200

    adding themeing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import "./App.scss";
+import React, { useState } from "react";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, purpleTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div`
+  text-align: center;
+  color: ${(props) => props.theme.fontColor};
+`;
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("purple");
+    }
+  };
   return (
-    <div className="App">
-      <div>Hello!</div>
-    </div>
+    <ThemeProvider
+      theme={
+        theme === "light"
+          ? lightTheme
+          : theme === "dark"
+          ? darkTheme
+          : purpleTheme
+      }
+    >
+      <GlobalStyles />
+      <StyledApp>
+        Hey there!
+        <button onClick={() => themeToggler()}>Switch Theme</button>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 

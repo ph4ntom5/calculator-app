c57416f20cd5d7228fb72a4388860770afaa920e
ph4ntom5 <tomsstals1@gmail.com>
2021-09-21 21:00:15 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,41 +1,87 @@
-import React, { useState } from "react";
-import "./App.css";
-import styled, { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme, purpleTheme, GlobalStyles } from "./themes.js";
+import React from "react";
+import styled from "styled-components";
+import { GlobalStyles } from "./globalstyles";
 
-const StyledApp = styled.div`
-  text-align: center;
-  color: ${(props) => props.theme.fontColor};
+const Wrapper = styled.div`
+  height: 100vh;
+  background-color: var(--bg);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-function App() {
-  const [theme, setTheme] = useState("light");
+const Card = styled.section`
+  margin-top: 8rem;
+  min-width: 80rem;
+  max-width: 85rem;
+  height: 40rem;
+  border-radius: 2rem;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
 
-  const themeToggler = () => {
-    if (theme === "light") {
-      setTheme("dark");
-    } else if (theme === "dark") {
-      setTheme("light");
-    } else {
-      setTheme("purple");
-    }
-  };
+const InputArea = styled.section`
+  height: 85%;
+  width: 45%;
+  background-color: white;
+  border-radius: 2rem;
+`;
+
+const Screen = styled.section`
+  height: 85%;
+  width: 45%;
+  border-radius: 2rem;
+  background-color: var(--very-dark-cyan);
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  width: 85%;
+  height: 5.5rem;
+  background-color: var(--button);
+  color: var(--very-dark-cyan);
+  cursor: pointer;
+  border: none;
+  border-radius: 0.6rem;
+  font-weight: bold;
+  font-family: "Space Mono";
+  font-size: 2.4rem;
+  transition: 0.4s;
+
+  &:hover {
+    background-color: var(--bg);
+    transition: 0.4s;
+  }
+`;
+
+const Headline = styled.text`
+  color: --dark2-gray-cyan;
+  opacity: 0.6;
+  letter-spacing: 1rem;
+  font-size: 2.4rem;
+  font-weight: 700;
+`;
+function App() {
   return (
-    <ThemeProvider
-      theme={
-        theme === "light"
-          ? lightTheme
-          : theme === "dark"
-          ? darkTheme
-          : purpleTheme
-      }
-    >
+    <Wrapper>
       <GlobalStyles />
-      <StyledApp>
-        Hey there!
-        <button onClick={() => themeToggler()}>Switch Theme</button>
-      </StyledApp>
-    </ThemeProvider>
+      <Headline>
+        SPLI
+        <br />
+        TTER
+      </Headline>
+      <Card>
+        <InputArea></InputArea>
+        <Screen>
+          <Button>RESET</Button>
+        </Screen>
+      </Card>
+    </Wrapper>
   );
 }
 
---
+++ src/globalstyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+html {
+    
+    --button: hsl(172, 66%, 46%);
+    --primary-color: hsl(172, 67%, 45%);  
+    --very-dark-cyan: hsl(183, 100%, 15%);
+    --dark-gray-cyan: hsl(186, 14%, 43%);
+    --dark2-gray-cyan: hsl(184, 14%, 56%);
+    --light-gray-cyan: hsl(189, 41%, 97%); 
+    --bg: hsl(185, 41%, 84%);
+    font-size: 62.5%;
+}
+`;
